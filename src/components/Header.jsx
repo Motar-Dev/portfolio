38b0d782abffc2e92a75f5dfefb935f728dcede5
Motar-Dev <mohammed.motar10@gmail.com>
2026-04-19 15:10:15 +0200

    install pkg and work with menu button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
+// Node modules
+import { useState } from 'react';
 
-/**
- * Components
- */
+// Components
 import Navbar from './Navbar';
 
 const Header = () => {
+  const [navOpen, setNavOpen] = useState(false);
+
   return (
     <header className='fixed top-0 left-0 w-full h-20 flex items-center z-40 bg-linear-to-b from-zinc-900 to-zinc-900/0'>
       <div className='max-w-screen-2xl w-full mx-auto px-4 flex justify-between items-center md:px-6 grid-cols-[1fr,3fr,1fr]'>
@@ -16,13 +17,21 @@ const Header = () => {
         </h1>
 
         <div className='relative md:justify-self-center'>
-          <button className='menu-btn md:hidden' onClick={null}>
-            <span className='material-symbols-rounded'>menu</span>
-          </button>
-          <Navbar />
+          <div className='md:hidden'>
+            <button
+              className='menu-btn'
+              onClick={() => setNavOpen((prev) => !prev)}
+            >
+              <span className='material-symbols-rounded'>menu</span>
+            </button>
+          </div>
+          <Navbar navOpen={navOpen} />
         </div>
 
-        <a href='#contact' className=''>
+        <a
+          href='#contact'
+          className='btn btn-secondary max-md:hidden md:justify-self-end'
+        >
           Contact Me
         </a>
       </div>
